cbf326e8666ebc66cbecbdbfd1c98434e019f0c7
Anna <acesbrama@gmail.com>
2024-06-22 12:04:28 -0300

    Começando o javascript e json

---
+++ produtos.js
@@ -0,0 +1,14 @@
+let productsJson = [
+    {
+        id: 1, name: 'Mouse Genérico (preto)', price: 15.90
+    },
+    {
+        id: 2, name: 'Teclado Genérico (preto)', price: 29.00
+    },
+    {
+        id: 3, name: 'Monitor Positivo 19', price: 350.00
+    },
+    {
+        id: 3, name: 'Cadeira Gamer Genérico (preto)', price: 250.00
+    }
+]
