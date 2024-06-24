89332513c6e74057906ec490087177d0f7b6a90c
Anna <acesbrama@gmail.com>
2024-06-24 12:39:31 -0300

    Botão limpar adicionado e outras funções iniciadas

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let btnAdd = document.querySelector('#add')
 let btnLimpar = document.querySelector('#limpar')
 let erro = document.querySelector('.erro')
-let list = document.querySelector('.products-list')
+let list = document.querySelector('.products-list') //lista de produtos selecionados
+let countBox = document.querySelector('#count-box') //caixa com números de quantidades pedidas
 
 const select = document.querySelector('#select')
 let arraySelect, product, price
 // console.log(product)
+
+let qtd = [] //quantidades dos produtos
+
+let total //soma dos produtos adicionados
 
 btnAdd.addEventListener('click', () => {
     if (select.value === '') {
@@ -21,8 +26,47 @@ function adicionar() {
     product = arraySelect[0]
     price = arraySelect[1]
 
+    qtd.push(countBox.value)
+    // countBox.value = ''
+
+    validateAmount()
     erro.textContent = ''
-    list.innerHTML = list.innerHTML + '<span>' + product + ' - ' + price + '</span>'
+    list.innerHTML = list.innerHTML + '<span>' + qtd + ' ' + product + ' - ' + price + '</span>'
+
+    resetQtd()
+
+    productsValue()
+
+
 }
 
+function validateAmount() {
+    if (countBox.value == '0') {
+        erro.textContent = 'ERRO - Insira uma quantidade válida'
+        return true
+    }
+    return false 
+}
 
+function resetQtd() {
+    while (qtd.length != 0) {
+        qtd.pop('')
+
+        countBox.value = ''
+    }
+}
+
+clean()
+
+function clean() {
+    btnLimpar.addEventListener('click', () => {
+        if (list.length != 0) {
+            list.innerHTML = ''
+        }
+    })
+}
+
+// function productsValue() {
+
+//     total = countBox + (qtd * price)
+// }
